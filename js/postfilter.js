f38991921ac1c3ebadcb6c290a6bc0b62621ef7a
LootGodamn <juneminhtet2007@gmail.com>
2024-02-03 00:12:44 +0630

    post browser design done

--- js/postfilter.js
+++ js/postfilter.js
@@ -2,6 +2,24 @@ filterSelection("all")
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("filterDiv");
+  var CategoryText = document.getElementById("categorydisplay");
+  switch(c){
+      case "none":
+        CategoryText.textContent = "Uncategorized";
+        break;
+      case "unreal":
+        CategoryText.textContent = "Unreal Engine";
+        break;
+      case "cpp":
+        CategoryText.textContent = "C++";
+        break;
+      case "py":
+        CategoryText.textContent = "Python";
+        break;
+      default:
+        CategoryText.textContent = "All Posts";
+        break;
+  }
   if (c == "all") c = "";
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
@@ -36,8 +54,8 @@ function w3RemoveClass(element, name) {
 }
 
 // Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
+var CategoryContainer = document.getElementById("categorycontainer");
+var btns = CategoryContainer.getElementsByClassName("category");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
